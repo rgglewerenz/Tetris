110b36e562b06aac3d806c74b9ffddd52a32bc5c
Ross Lewerenz <rgglewerenz@gmail.com>
2022-03-25 21:57:11 -0400

    all 4 rotations for tblock initially implemented with no bug fixing

--- TBlock.cpp
+++ TBlock.cpp
@@ -349,7 +349,7 @@ void TBlock::rotate(bool directon) {
 	}
 	case 2:
 	{
-		if (__xR / __size < __maxX && __xL / __size > 1) {
+		if (__xR / __size < __maxX - 1 && __xL / __size > 1) {
 			placeXY(false);
 			__xR += __size;
 			Blocks[0].setX(0);
@@ -403,7 +403,7 @@ void TBlock::rotate(bool directon) {
 			rotation++;
 			break;
 		}
-		else if (__xR / __size < __maxX && !(__xL / __size > 1)) {
+		else if (__xR / __size < __maxX - 1 && !(__xL / __size > 1)) {
 			placeXY(false);
 			__x += __size;
 			__xR += __size;
@@ -462,9 +462,9 @@ void TBlock::rotate(bool directon) {
 		}
 		else {
 			placeXY(false);
-			__x -= __size;
-			__xR -= __size;
-			__xL -= __size;
+			__x += __size;
+			__xR += __size;
+			__xL += __size;
 			__xR += __size;
 			Blocks[0].setX(0);
 			Blocks[0].setY(0);
@@ -477,6 +477,177 @@ void TBlock::rotate(bool directon) {
 
 			Blocks[3].setX(0);
 			Blocks[3].setY(-1 * __size);
+			if (checkOverlap(grid) || __y / __size > __maxY - 1) {
+				if (checkOverlap(grid)) {
+					Blocks[0].setX(0);
+					Blocks[0].setY(0);
+
+					Blocks[1].setX(1 * __size);
+					Blocks[1].setY(1 * __size);
+
+					Blocks[2].setX(0);
+					Blocks[2].setY(1 * __size);
+
+					Blocks[3].setX(0);
+					Blocks[3].setY(2 * __size);
+					placeXY(true);
+					break;
+				}
+				if ((__y / __size < __maxY - 2)) {
+					__y -= __size;
+					if (checkOverlap(grid) || __y / __size > __maxY - 1) {
+						Blocks[0].setX(0);
+						Blocks[0].setY(0);
+
+						Blocks[1].setX(1 * __size);
+						Blocks[1].setY(1 * __size);
+
+						Blocks[2].setX(0);
+						Blocks[2].setY(1 * __size);
+
+						Blocks[3].setX(0);
+						Blocks[3].setY(2 * __size);
+						__y += __size;
+						placeXY(true);
+						break;
+					}
+				}
+			}
+			placeXY(true);
+			rotation++;
+			break;
+		}
+	}
+	case 3:
+	{
+		if (__xR / __size < __maxX && __xL / __size > 1) {
+			placeXY(false);
+			__xL += __size;
+			Blocks[0].setX(0);
+			Blocks[0].setY(0);
+
+			Blocks[1].setX(1 * __size);
+			Blocks[1].setY(1 * __size);
+
+			Blocks[2].setX(0);
+			Blocks[2].setY(1 * __size);
+
+			Blocks[3].setX(0);
+			Blocks[3].setY(2 * __size);
+			if (checkOverlap(grid) || __y / __size > __maxY - 1) {
+				if (checkOverlap(grid)) {
+					Blocks[0].setX(0);
+					Blocks[0].setY(0);
+
+					Blocks[1].setX(1 * __size);
+					Blocks[1].setY(1 * __size);
+
+					Blocks[2].setX(0);
+					Blocks[2].setY(1 * __size);
+
+					Blocks[3].setX(0);
+					Blocks[3].setY(2 * __size);
+					placeXY(true);
+					break;
+				}
+				if ((__y / __size < __maxY - 2)) {
+					__y -= __size;
+					if (checkOverlap(grid) || __y / __size > __maxY - 1) {
+						Blocks[0].setX(0);
+						Blocks[0].setY(0);
+
+						Blocks[1].setX(1 * __size);
+						Blocks[1].setY(1 * __size);
+
+						Blocks[2].setX(0);
+						Blocks[2].setY(1 * __size);
+
+						Blocks[3].setX(0);
+						Blocks[3].setY(2 * __size);
+						__y += __size;
+						placeXY(true);
+						break;
+					}
+				}
+			}
+			placeXY(true);
+			rotation++;
+			break;
+		}
+		else if (__xR / __size < __maxX && !(__xL / __size > 1)) {
+			placeXY(false);
+			__x += __size;
+			__xR += __size;
+			__xL += __size;
+			__xL += __size;
+			Blocks[0].setX(0);
+			Blocks[0].setY(0);
+
+			Blocks[1].setX(1 * __size);
+			Blocks[1].setY(1 * __size);
+
+			Blocks[2].setX(0);
+			Blocks[2].setY(1 * __size);
+
+			Blocks[3].setX(0);
+			Blocks[3].setY(2 * __size);
+			if (checkOverlap(grid) || __y / __size > __maxY - 1) {
+				if (checkOverlap(grid)) {
+					Blocks[0].setX(0);
+					Blocks[0].setY(0);
+
+					Blocks[1].setX(1 * __size);
+					Blocks[1].setY(1 * __size);
+
+					Blocks[2].setX(0);
+					Blocks[2].setY(1 * __size);
+
+					Blocks[3].setX(0);
+					Blocks[3].setY(2 * __size);
+					placeXY(true);
+					break;
+				}
+				if ((__y / __size < __maxY - 2)) {
+					__y -= __size;
+					if (checkOverlap(grid) || __y / __size > __maxY - 1) {
+						Blocks[0].setX(0);
+						Blocks[0].setY(0);
+
+						Blocks[1].setX(1 * __size);
+						Blocks[1].setY(1 * __size);
+
+						Blocks[2].setX(0);
+						Blocks[2].setY(1 * __size);
+
+						Blocks[3].setX(0);
+						Blocks[3].setY(2 * __size);
+						__y += __size;
+						placeXY(true);
+						break;
+					}
+				}
+			}
+			placeXY(true);
+			rotation++;
+			break;
+		}
+		else {
+			placeXY(false);
+			__x -= __size;
+			__xR -= __size;
+			__xL -= __size;
+			__xL += __size;
+			Blocks[0].setX(0);
+			Blocks[0].setY(0);
+
+			Blocks[1].setX(1 * __size);
+			Blocks[1].setY(1 * __size);
+
+			Blocks[2].setX(0);
+			Blocks[2].setY(1 * __size);
+
+			Blocks[3].setX(0);
+			Blocks[3].setY(2 * __size);
 			if (checkOverlap(grid) || __y / __size > __maxY - 1) {
 				if (checkOverlap(grid)) {
 					Blocks[0].setX(0);
